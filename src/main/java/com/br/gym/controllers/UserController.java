90cd16da0103cb1c77e05d17d0d67c6e8d86e25e
LeonardoGarciah <leonardoformulaum@hotmail.com>
2024-04-06 19:19:15 -0300

    feat(main): created users table

---
+++ src/main/java/com/br/gym/controllers/UserController.java
@@ -0,0 +1,58 @@
+package com.br.gym.controllers;
+
+import com.br.gym.dtos.UserRecordDto;
+import com.br.gym.models.UserModel;
+import com.br.gym.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+public class UserController {
+    final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+        UserModel user = new UserModel();
+
+        BeanUtils.copyProperties(userRecordDto, user);
+
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não foi possivel criar o usuário");
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "id") UUID id, @RequestBody @Valid UserRecordDto userRecordDto) {
+        Optional<UserModel> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+
+        UserModel userModel = user.get();
+
+        BeanUtils.copyProperties(userRecordDto, userModel);
+
+        return ResponseEntity.ok(userRepository.save(userModel));
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable(value = "id") UUID id) {
+        Optional<UserModel> user = userRepository.findById(id);
+
+        return user.<ResponseEntity<Object>>map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found."));
+
+    }
+}
---
+++ src/main/java/com/br/gym/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.br.gym.repositories;
+
+import com.br.gym.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+}
